e838254b7e43ea73daf3c615a6501c274e1aaea7
Me <pdprince.lfc+github@gmail.com>
2023-11-22 16:28:36 -0500

    that's it??

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const result = [];
+    for (let name of names) {
+        result.push(`Thank you, ${name}, for the wonderful ${event} gift!`)
+    }
+    return result;
+}
+
+function countDown(start) {
+    for (let i = start; i >= 0; i--) {
+        console.log(i);
+    }
+}
